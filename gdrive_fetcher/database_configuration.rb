cdfba613ca04a06da4a2b69601ca9c7250f585c0
Victor Ivanov <vic.ivanoff@gmail.com>
2013-07-22 23:01:57 +0400

    db settings

--- gdrive_fetcher/database_configuration.rb
+++ gdrive_fetcher/database_configuration.rb
@@ -1,6 +1,13 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
+settings = YAML.load_file('fetcher_settings.yml')
+
+db_file_path = File.join(File.dirname(__FILE__), 'database.yml')
+database = YAML::load_file(db_file_path)
+
+env = settings['mode'] == 'dev' ? 'development' : 'production'
+
+ActiveRecord::Base.establish_connection(database[env])
     :adapter => "postgresql",
     :host => "localhost",
     :database => "mps_development",
