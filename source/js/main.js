8e36fd151d0109ed9a4d96e9bbf18d87dc835b31
Victor Ivanov <vic.ivanoff@gmail.com>
2013-07-29 16:11:55 +0400

    Прееключился на амазоновский сервак

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,8 @@ $("#search-field").keyup(function(e){
     if (timer) clearTimeout(timer);
     timer = setTimeout(function(){
         var q = $("#search-field").val();
-        //$.getJSON("http://ec2-54-217-232-182.eu-west-1.compute.amazonaws.com/articles/"+q+".json",$.getJSON("http://ec2-54-217-232-182.eu-west-1.compute.amazonaws.com/articles/"+q+".jsonp?callback=?",
-        $.getJSON("http://localhost:3000/articles/"+q+".jsonp?callback=?",
+        $.getJSON("http://ec2-54-217-232-182.eu-west-1.compute.amazonaws.com/articles/"+q+".json",$.getJSON("http://ec2-54-217-232-182.eu-west-1.compute.amazonaws.com/articles/"+q+".jsonp?callback=?",
+        //$.getJSON("http://localhost:3000/articles/"+q+".jsonp?callback=?",
 
             {
             },
