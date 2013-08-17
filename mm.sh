d773d53294a1d81818906eeb9b19f8c98201868a
Victor Ivanov <vic.ivanoff@gmail.com>
2013-08-17 22:40:08 +0400

    Переехали в mp3

--- mm.sh
+++ mm.sh
@@ -8,4 +8,4 @@ bundle exec ruby ./gdrive_fetcher/gdrive_fetcher.rb
 middleman build --clean
 
 touch build/texts build/glossariy
-scp  -r build/* u48777@u48777.ssh.masterhost.ru:~/gerome.ru/www/mp2
+scp  -r build/* u48777@u48777.ssh.masterhost.ru:~/gerome.ru/www/mp3
