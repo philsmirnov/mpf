2a327130f51f778a926dab622cb6a8a1c6b256f7
Victor Ivanov <vic.ivanoff@gmail.com>
2013-08-23 16:34:36 +0400

    забыл JS для шрифтов

---
+++ source/js/about.js
@@ -0,0 +1,22 @@
+$(function(){
+    resize = function(){
+        var fontSize = localStorage.getItem('fontSize');
+        if (fontSize) {
+            $('.app_fonts .columns').removeClass('active')
+            if (fontSize == "0.875em") $('.app_fonts .small-font').addClass('active');
+            if (fontSize == "1.05em") $('.app_fonts .mid-font').addClass('active');
+            if (fontSize == "1.2em") $('.app_fonts .big-font').addClass('active');
+        }
+    };
+
+    $('.app_fonts .columns').click(function(e){
+        var div = $(e.target).parent('div');
+        if (div.hasClass('small-font')) localStorage.setItem('fontSize', "0.875em");
+        if (div.hasClass('mid-font')) localStorage.setItem('fontSize', "1.05em");
+        if (div.hasClass('big-font')) localStorage.setItem('fontSize', "1.2em");
+        resize();
+        MBP.setFontSize();
+    });
+
+    resize();
+});
