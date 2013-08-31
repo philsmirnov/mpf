ea3d5fb9752362047ba0ede6db6ab394804ff676
Victor Ivanov <vic.ivanoff@gmail.com>
2013-08-31 19:57:09 +0400

    Чистим директорию перед копированием

--- mm.sh
+++ mm.sh
@@ -8,4 +8,6 @@ bundle exec middleman build --clean
 
 touch build/texts build/glossariy
 
+ssh u48777@u48777.ssh.masterhost.ru rm -rf ~/gerome.ru/www/mp3/*
+
 scp  -r build/* u48777@u48777.ssh.masterhost.ru:~/gerome.ru/www/mp3
