d3a03ee642d093014beea22fd0c329b062a19d4e
Victor Ivanov <vic.ivanoff@gmail.com>
2013-09-18 15:58:55 +0400

    Переезжаем на новый домен app.papush.ru

--- mm.sh
+++ mm.sh
@@ -9,8 +9,10 @@ bundle exec middleman build --clean
 touch build/texts build/glossariy
 
 ssh u48777@u48777.ssh.masterhost.ru rm -rf ~/gerome.ru/www/mp3/*
-
 scp  -r build/. u48777@u48777.ssh.masterhost.ru:~/gerome.ru/www/mp3
+
+ssh papush@ssh.papush.nichost.ru rm -rf ~/app.papush.ru/docs/*
+scp  -r build/. papush@ssh.papush.nichost.ru:~/app.papush.ru/docs
 
 cd ../../../mps/current/
 RAILS_ENV=production bundle exec rake ts:rebuild
